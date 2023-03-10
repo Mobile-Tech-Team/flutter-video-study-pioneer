c25e167ff6b51c1828fd59d51bc46e5026a6fac7
Pioneer-Super-Dev <pioneer0221@outlook.com>
2023-03-10 18:58:42 +0200

    product add button widget is added

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,11 +1,14 @@
 import "package:flutter/material.dart";
 
 import "./product.dart";
+import "./product_control.dart";
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
 
-  ProductManager(this.startingProduct);
+  ProductManager({this.startingProduct = "Sweet Tester"}) {
+    print('[ProductManager Widget] Constructor');
+  }
 
   @override
   State<StatefulWidget> createState() {
@@ -24,20 +27,25 @@ class _ProductManagerState extends State<ProductManager> {
   }
 
   @override
+  void didUpdateWidget(covariant ProductManager oldWidget) {
+    print("[ProductManager State] didUpdateWidget()");
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Column(
       children: [
         Container(
           margin: EdgeInsets.all(10.0),
-          child: FilledButton(
-            onPressed: () {
-              setState(() {
-                _products.add("Advanced Beauty Tester");
-              });
-            },
-            child: Text("Add Product"),
-          ),
+          child: ProductControl(_addProduct),
         ),
         Products(_products)
       ],
